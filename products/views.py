d8f3d80296dff068225ab90e7b57727c1f2c1f71
slucaci <serjlucaci@gmail.com>
2025-01-10 20:50:12 +0000

    Implemented individual product detail view

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
+
 
 # Create your views here.
 
@@ -15,13 +16,13 @@ def all_products(request):
     return render(request, 'products/products.html', context)
 
 
-def product_detail(request):
+def product_detail(request, product_id):
     """ A view to show individual product details """
 
-    products = Product.objects.all()
+    product = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'products': products,
+        'product': product,
     }
 
-    return render(request, 'products/products.html', context)
+    return render(request, 'products/product_detail.html', context)
