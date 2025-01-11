f946f85354389d7421ecfd3f0f2a5d695831c824
slucaci <serjlucaci@gmail.com>
2025-01-11 18:37:34 +0000

    Implemented profile form, views and templates

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
-@login_required
+from .models import UserProfile
+from .forms import UserProfileForm
+
+
 def profile(request):
-    """ A view to display the user's profile """
-    return render(request, 'profiles/profile.html')
+    """ Display the user's profile. """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
+    template = 'profiles/profile.html'
+    context = {
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True
+    }
+
+    return render(request, template, context)
