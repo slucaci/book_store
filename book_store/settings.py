da8dec965d10b5841ac964fd3a7f5f0e0559e590
slucaci <serjlucaci@gmail.com>
2025-01-11 22:52:32 +0000

    deployment

--- book_store/settings.py
+++ book_store/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sergiulucaci-bookstore.herokuapp.com', 'localhost','127.0.0.1']
+ALLOWED_HOSTS = ['book-store.herokuapp.com', 'localhost','127.0.0.1']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
