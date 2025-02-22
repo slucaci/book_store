8132bbfc264e2be5cb10a7ddaa38e1f3a59bdfcd
slucaci <serjlucaci@gmail.com>
2025-02-22 13:09:06 +0000

    Removed wishlist from userprofile model

--- profiles/models.py
+++ profiles/models.py
@@ -22,7 +22,6 @@ class UserProfile(models.Model):
     default_county = models.CharField(max_length=80, null=True, blank=True)
     default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_country = models.CharField(default='IE',max_length=40, null=False, blank=False)
-    wishlist = models.ManyToManyField(Product, blank=True)
 
     def __str__(self):
         return self.user.username
