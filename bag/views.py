1c4cbef3e0d1e4335c2396a2a08d41b336144341
slucaci <serjlucaci@gmail.com>
2025-05-05 18:11:17 +0100

    Tested form action and link functionality for add-to-bag process

--- bag/views.py
+++ bag/views.py
@@ -26,6 +26,7 @@ def add_to_bag(request, item_id):
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    print(f"Redirect URL: {redirect_url}")
     bag = request.session.get('bag', {})
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
