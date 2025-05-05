c5040e687fb28b791f1a6f7b88495b5679267f43
slucaci <serjlucaci@gmail.com>
2025-05-05 17:52:57 +0100

    Changed urls file to check for broken links

--- bag/urls.py
+++ bag/urls.py
@@ -4,7 +4,7 @@ from .views import apply_loyalty_points
 
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
-    path('bag/add/<item_id>/', views.add_to_bag, name='add_to_bag'),
+    path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
     path('adjust/<item_id>/', views.adjust_bag, name='adjust_bag'),
     path('remove/<item_id>/', views.remove_from_bag, name='remove_from_bag'),
     path('apply-loyalty/', apply_loyalty_points, name='apply_loyalty_points'),
