394ab0df4e5c39f962c3a65ae3ed7745373a8fd7
slucaci <serjlucaci@gmail.com>
2025-05-05 18:17:44 +0100

    Fix TypeError in add_to_bag view by handling missing or invalid quantity

--- bag/views.py
+++ bag/views.py
@@ -24,7 +24,11 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
     product = get_object_or_404(Product, pk=item_id)
-    quantity = int(request.POST.get('quantity'))
+    quantity = request.POST.get('quantity', 1)
+    try:
+        quantity = int(quantity)
+    except ValueError:
+        quantity = 1 
     redirect_url = request.POST.get('redirect_url')
     print(f"Redirect URL: {redirect_url}")
     bag = request.session.get('bag', {})
