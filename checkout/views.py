5c19157362319f13683a0e0cc6d21ec7baf261e4
slucaci <serjlucaci@gmail.com>
2025-05-07 18:39:20 +0100

    Implemented order confirmation email functionality. Added a function to send a confirmation email to customers upon successful checkout

--- checkout/views.py
+++ checkout/views.py
@@ -10,8 +10,32 @@ from profiles.models import UserProfile
 from profiles.forms import UserProfileForm
 from profiles.models import LoyaltyPoints 
 from decimal import Decimal
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+
 
 import stripe
+
+def send_confirmation_email(order):
+        """Send the user a confirmation email"""
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order})
+        message = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL}
+            )
+        print(subject)
+        print(message)
+        
+        send_mail(
+            subject,
+            message,
+            settings.DEFAULT_FROM_EMAIL,
+            [order.email],
+            fail_silently=False,
+        )
+        print("test function send_confirmation_email()")
 
 
 def checkout(request):
@@ -152,7 +176,7 @@ def checkout_success(request, order_number):
             user_profile_form = UserProfileForm(profile_data, instance=profile)
             if user_profile_form.is_valid():
                 user_profile_form.save()
-
+    send_confirmation_email(order)
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. You earned {points_earned} loyalty points!')
 
